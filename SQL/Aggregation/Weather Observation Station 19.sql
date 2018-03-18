8da459a24dd7a3c4938d06c2b705b347b20a3d31
Ravi Kiran Mandha <35540968+CuriousMind007@users.noreply.github.com>
2018-03-18 14:50:25 -0400

    Weather Observation Station 19
    https://www.hackerrank.com/challenges/weather-observation-station-19/problem

---
+++ SQL/Aggregation/Weather Observation Station 19.sql
@@ -0,0 +1,6 @@
+--     Author: Ravi Kiran Mandha
+--     Github: github.com/CuriousMind007
+-- HackerRank: hackerrank.com/CuriousMind049
+
+
+SELECT ROUND(SQRT(POWER((MAX(LAT_N)-MIN(LAT_N)),2) + POWER((MAX(LONG_W)-MIN(LONG_W)),2)),4) FROM STATION;
