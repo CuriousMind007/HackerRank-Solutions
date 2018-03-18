31956582f4133d3712310533ba28dded2d0dcbdb
Ravi Kiran Mandha <35540968+CuriousMind007@users.noreply.github.com>
2018-03-18 14:55:18 -0400

    Weather Observation Station 16
    https://www.hackerrank.com/challenges/weather-observation-station-16/problem

---
+++ SQL/Aggregation/Weather Observation Station 16.sql
@@ -0,0 +1,6 @@
+--     Author: Ravi Kiran Mandha
+--     Github: github.com/CuriousMind007
+-- HackerRank: hackerrank.com/CuriousMind049
+
+
+SELECT ROUND(MIN(LAT_N),4) FROM STATION WHERE (LAT_N) > 38.7780;
