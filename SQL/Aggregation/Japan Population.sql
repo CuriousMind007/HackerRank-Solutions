190a7d48e1d3fdcb05826928d8494461d390c15e
Ravi Kiran Mandha <35540968+CuriousMind007@users.noreply.github.com>
2018-03-18 14:48:06 -0400

    Japan Population
    https://www.hackerrank.com/challenges/japan-population/problem

---
+++ SQL/Aggregation/Japan Population.sql
@@ -0,0 +1,6 @@
+--     Author: Ravi Kiran Mandha
+--     Github: github.com/CuriousMind007
+-- HackerRank: hackerrank.com/CuriousMind049
+
+
+SELECT SUM(POPULATION) FROM CITY WHERE COUNTRYCODE ='JPN';
