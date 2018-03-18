9c8a2189beb0963f063d18f8f6e0e4b71cfa7dad
Ravi Kiran Mandha <35540968+CuriousMind007@users.noreply.github.com>
2018-03-18 14:51:21 -0400

    Weather Observation Station 2
    https://www.hackerrank.com/challenges/weather-observation-station-2/problem

---
+++ SQL/Aggregation/Weather Observation Station 2.sql
@@ -0,0 +1,6 @@
+--     Author: Ravi Kiran Mandha
+--     Github: github.com/CuriousMind007
+-- HackerRank: hackerrank.com/CuriousMind049
+
+
+SELECT ROUND(SUM(LAT_N),2), ROUND(SUM(LONG_W),2) FROM STATION;
