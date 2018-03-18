4311bd1954a6d77fdea9769c1047176c36aac72d
Ravi Kiran Mandha <35540968+CuriousMind007@users.noreply.github.com>
2018-03-18 14:52:13 -0400

    Weather Observation Station 13
    https://www.hackerrank.com/challenges/weather-observation-station-13/problem

---
+++ SQL/Aggregation/Weather Observation Station 13.sql
@@ -0,0 +1,6 @@
+--     Author: Ravi Kiran Mandha
+--     Github: github.com/CuriousMind007
+-- HackerRank: hackerrank.com/CuriousMind049
+
+
+SELECT ROUND(SUM(LAT_N),4) FROM STATION WHERE LAT_N BETWEEN 38.7880 AND 137.2345;
